95e362e58129237173e271e46f841347abaeb68f
ananyakushal <ananyakushal15@gmail.com>
2024-02-10 19:25:13 +0530

    Configured few merge conflicts, Added components(navbar,intro)

---
+++ sankalp_web/src/App.js
@@ -0,0 +1,24 @@
+import Navbar from "./components/Navbar/Navbar";
+import "./App.css";
+import { useContext } from "react";
+import { themeContext } from "./Context";
+import Intro from "./components/Intro/Intro";
+
+function App() {
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+  return (
+    <div
+      className="App"
+      style={{
+        background: darkMode ? "black" : "",
+        color: darkMode ? "white" : "",
+      }}
+    >
+      <Navbar />
+      <Intro/>
+    </div>
+  );
+}
+
+export default App;
