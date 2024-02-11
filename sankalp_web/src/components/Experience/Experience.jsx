7a4fd75c54500724dd77809bde62f7d64d55fe13
ananyakushal <ananyakushal15@gmail.com>
2024-02-11 05:59:10 +0530

    Added images

--- sankalp_web/src/components/Experience/Experience.jsx
+++ sankalp_web/src/components/Experience/Experience.jsx
@@ -9,19 +9,19 @@ const Experience = () => {
     <div className="experience" id='experience'>
       <div className="achievement">
         {/* darkMode */}
-        <div className="circle" style={{color: darkMode?'var(--orange)':''}}>8+</div>
-        <span  style={{color: darkMode?'white':''}}>years </span>
-        <span>Experience</span>
+        <div className="circle" style={{color: darkMode?'var(--orange)':''}}>9.4L+</div>
+        <span  style={{color: darkMode?'white':''}}> </span>
+        <span>Cocain</span>
       </div>
       <div className="achievement">
-        <div className="circle" style={{color: darkMode?'var(--orange)':''}}>20+</div>
-        <span  style={{color: darkMode?'white':''}}>completed </span>
-        <span>Projects</span>
+        <div className="circle" style={{color: darkMode?'var(--orange)':''}}>70%</div>
+        <span  style={{color: darkMode?'white':''}}> </span>
+        <span>Suicides</span>
       </div>
       <div className="achievement">
-        <div className="circle" style={{color: darkMode?'var(--orange)':''}}>5+</div>
-        <span  style={{color: darkMode?'white':''}}>companies </span>
-        <span>Work</span>
+        <div className="circle" style={{color: darkMode?'var(--orange)':''}}>98.6%</div>
+        <span  style={{color: darkMode?'white':''}}> </span>
+        <span>Considers serious</span>
       </div>
     </div>
   );
